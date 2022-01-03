9ffd6d673301321eee34701762bbf7e4be5c8c5f
hyeju <hae7077@naver.com>
2022-01-03 15:26:35 +0900

    feat: first commit

---
+++ src/main/java/solux/wansuki/OurNeighbor_BE/OurNeighborBeApplication.java
@@ -0,0 +1,13 @@
+package solux.wansuki.OurNeighbor_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurNeighborBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurNeighborBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solux/wansuki/OurNeighbor_BE/OurNeighborBeApplicationTests.java
@@ -0,0 +1,13 @@
+package solux.wansuki.OurNeighbor_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurNeighborBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
